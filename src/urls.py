3ad3bbb88da2729bc97b2232e676a03509f0abd4
Matthew J Morrison <mattj.morrison@gmail.com>
2010-12-16 00:10:10 -0600

    added ability for a single application to handle multiple separate flows

--- src/urls.py
+++ src/urls.py
@@ -2,6 +2,6 @@ from django.conf.urls.defaults import *
 from example.flows import SampleFlow, OtherFlow
 
 urlpatterns = patterns('',
-    url(r'^first/', include(SampleFlow().urls), name="first"),
-    url(r'^second/', include(OtherFlow().urls), name="second"),
+    url(r'^first/', include(SampleFlow().urls, namespace="first"), name="first"),
+    url(r'^second/', include(OtherFlow().urls, namespace="second"), name="second"),
 )
